de67568d88b5002f37b6bdc842a9fc3190e3415a
maleezy <bmalunga28@gmail.com>
2024-05-01 20:04:06 +0200

    Formating the label

--- hello.py
+++ hello.py
@@ -4,8 +4,15 @@ root = Tk()
 root.title("Calculator")
 root.geometry("400x400")
 
-myVariable = Label(root,text="Hello world")
-myVariable.pack()
+#formatting text on a label fg for formating text colour 
+#bg or background fir formatting background colour 
+# font(font family, size)
+# height=() changing 
+
+myVariable = Label(root,text="Hello world", fg="white", bg="red" , width=50, font=("Helvatica",30), height=2)
+#pady(20) moving the label 20 pixels along the y axies
+##padx(20) moving the label 20 pixels along the x axies
+myVariable.pack(pady=10)
 
 
 root.mainloop()
