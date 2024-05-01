effccf512e6ee4938a5627614ebb62ccb377ae1c
maleezy <bmalunga28@gmail.com>
2024-05-01 21:22:08 +0200

    Using Buttons

--- hello.py
+++ hello.py
@@ -4,6 +4,13 @@ root = Tk()
 root.title("Calculator")
 root.geometry("400x400")
 
+#Adding Clicked Function whic is handled by a button 
+def Clicked():
+    myBtn= Label(root,text="Welcome to Buttons creation").grid(row=4,column=0)
+
+def clicked():
+   myBtn= Label(root,text="Welcome to Buttons creation").grid(row=4,column=0) 
+
 #formatting text on a label fg for formating text colour 
 #bg or background fir formatting background colour 
 # font(font family, size)
@@ -19,4 +26,10 @@ myVariable2.grid(row=2,column=1,sticky=E)
 
 myVariable3 = Label(root,text="Subtraction")
 myVariable3.grid(row=3,column=1,sticky=W)
+
+#Adding a button to our program // you can join pack or grid at the end of Button widget
+myButton = Button(root,text="Pliz Click Me",command=Clicked).grid(row=3,column=3,sticky=S)
+
+#adding button thats have a function with paramters using lamdba:
+myButton2 = Button(root,text="Click to Display",command=clicked).grid(row=3,column=4)
 root.mainloop()
